4e3067ae586c3f5aec8f68e029ba333f72426a05
Sakib Hassan <msakibhr@gmail.com>
2020-08-04 17:57:20 +0600

    Step-2 done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!')
+const generatorDisplay = document.querySelector('.pin-generator input');
+const generateButton = document.querySelector('.generate-btn');
+
+let pin = 0;
+
+generateButton.addEventListener('click', () => {
+	pin = Math.floor(1000 + Math.random() * 9000);
+	generatorDisplay.value = pin;
+});
